3eef6e8859c5984ca587fa5d4d948f0a7b7840c0
Tanmoy Banik <imtanmoybanik@gmail.com>
2018-12-27 14:16:02 +0600

    11

--- app/tasks/task.py
+++ app/tasks/task.py
@@ -3,14 +3,49 @@ import random
 import time
 import pprint
 from flask import current_app
-from celery.signals import task_postrun
+from celery.signals import task_postrun, worker_process_init, task_prerun
 from celery.utils.log import get_task_logger
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 
-from app import celery, db
+from app import celery, db, create_app
+from app.models.column import Column
 from app.models.database import Database
+from app.models.foreignkey import ForeignKey
 from app.models.message import Message
+from app.models.table import Table
 
 logger = get_task_logger(__name__)
+
+Session = scoped_session(sessionmaker(autocommit=False, autoflush=False))
+
+
+class SQLASessionTask(celery.Task):
+    _session = None
+
+    def __call__(self, *args, **kwargs):
+        print("CALLED!")
+        return super(SQLASessionTask, self).__call__(*args, **kwargs)
+
+    @property
+    def session(self):
+        app = current_app
+        print(app.config['SQLALCHEMY_DATABASE_URI'])
+        if self._session is None:
+            engine = create_engine(
+                app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True, echo_pool=True)
+            self._session = Session(bind=engine)
+        return self._session
+
+    def on_success(self, retval, task_id, args, kwargs):
+        print('success')
+        Session.remove()
+        # pass
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        print('failure')
+        Session.remove()
+        # pass
 
 
 @celery.task(bind=True)
@@ -55,15 +90,54 @@ def reverse_messages():
         db.session.commit()
 
 
-@celery.task
-def save_metadata(db_id):
+@celery.task(base=SQLASessionTask, bind=True)
+def save_metadata(self, db_id):
     """Reverse all messages in DB"""
     print('calling save meta data')
     pprint.pprint(db_id)
-    database = Database.query.get(db_id)
-    tables = database.save_remote_tables
-    for table in tables:
-        columns = table.save_remote_columns
+    db_session = self.session
+    # database = Database.query.get(db_id)
+    database = db_session.query(Database).get(db_id)
+    pprint.pprint(database)
+    # tables = database.save_remote_tables
+    new_tables = []
+    for tt in database.get_remote_tables:
+        pprint.pprint(getattr(tt, 'name'))
+        table_db = Table(table_name=getattr(tt, 'name'))
+        database.tables.append(table_db)
+        db_session.add(table_db)
+        db_session.commit()
+        new_tables.append(table_db)
+    print(new_tables)
+    for table in new_tables:
+        new_columns = []
+        for column in table.get_remote_columns:
+            ncol = Column(column_name=getattr(column, 'name'),
+                          column_type=getattr((getattr(column, 'type')), '__visit_name__'),
+                          column_default=getattr(column, 'default'),
+                          is_nullable=getattr(column, 'nullable'),
+                          is_autoincrement=False if getattr(column, 'autoincrement') == 'auto' else True,
+                          is_pk=getattr(column, 'primary_key'))
+            table.columns.append(ncol)
+            db_session.add(ncol)
+            db_session.commit()
+            new_columns.append(ncol)
+            fks = getattr(column, 'foreign_keys')
+            if len(fks) > 0:
+                parent = str(getattr(next(iter(fks)), '_colspec')).split('.')[0]
+                # parent_table = Table.query.filter_by(table_name=parent, database_id=db_id).first()
+                parent_table = db_session.query(Table).filter(Table.table_name == parent,
+                                                              Table.database_id == db_id).first()
+                # scol = Column.query.filter_by(column_name=ncol.column_name, table_id=parent_table.id).first()
+                scol = db_session.query(Column).filter(Column.column_name == ncol.column_name,
+                                                       Column.table_id == parent_table.id).first()
+                fk_key = ForeignKey(column_id=ncol.id, table_id=ncol.table_id, referred_column_id=scol.id,
+                                    referred_table_id=scol.table_id)
+                db_session.add(fk_key)
+                db_session.commit()
+    # return new_tables
+    # for table in tables:
+    #     columns = table.save_remote_columns
 
 
 @task_postrun.connect
@@ -72,4 +146,17 @@ def close_session(*args, **kwargs):
     # a scoped session factory, given that we are maintaining the same app
     # context, this ensures tasks have a fresh session (e.g. session errors
     # won't propagate across tasks)
-    db.session.remove()
+    print('removing session')
+    # db.session.remove()
+
+
+@task_prerun.connect
+def pre_run(*args, **kwargs):
+    print('before run')
+
+
+@worker_process_init.connect
+def init_celery_flask_app(**kwargs):
+    print('worker init')
+    # app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    # app.app_context().push()
--- celery_worker.py
+++ celery_worker.py
@@ -13,20 +13,21 @@ if os.path.exists(dotenv_path):
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
+app.app_context().push()
+
 
 def create_celery(app):
-    celery = Celery(app.import_name,
-                    backend=app.config['CELERY_RESULT_BACKEND'],
-                    broker=app.config['BROKER_URL'])
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
     celery.conf.update(app.config)
-    TaskBase = celery.Task
 
-    class ContextTask(TaskBase):
-        abstract = True
-
+    class ContextTask(celery.Task):
         def __call__(self, *args, **kwargs):
             with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
+                return self.run(*args, **kwargs)
 
     celery.Task = ContextTask
     return celery
